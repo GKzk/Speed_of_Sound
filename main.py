e801a3d1742968c14a911dbd727a13ba364b7757
GKzk <167706940+GKzk@users.noreply.github.com>
2026-09-16 20:43:21 +0300

    Update is_friday_morning condition for accuracy

--- main.py
+++ main.py
@@ -434,7 +434,7 @@ def main():
         return
 
     now = datetime.datetime.now()
-    is_friday_morning = True
+    is_friday_morning = (now.weekday() == 4) and (8 <= now.hour < 12)
 
     # 1. ПЯТНИЧНЫЙ ДАЙДЖЕСТ
     if is_friday_morning:
